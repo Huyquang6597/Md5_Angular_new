452e32ff2051f43f666bd8dc9d622a09e6f992b9
huyquang6597 <quanghuy6597@gmail.com>
2022-07-18 09:38:40 +0700

    done count down timer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,19 @@ import { ArticleComponent } from './article/article.component';
 import {FormsModule} from "@angular/forms";
 import { LikeComponent } from './like/like.component';
 import { NameCardComponent } from './name-card/name-card.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
+import { RatingBarComponent } from './rating-bar/rating-bar.component';
+import { CountdownTimerComponent } from './countdown-timer/countdown-timer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArticleComponent,
     LikeComponent,
-    NameCardComponent
+    NameCardComponent,
+    ProgressBarComponent,
+    RatingBarComponent,
+    CountdownTimerComponent
   ],
     imports: [
         BrowserModule,
